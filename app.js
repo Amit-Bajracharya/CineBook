0d81c4f9f0a69da6a66bb609429533d781556a72
Khonshu <amitbajracharya4444@gmail.com>
2026-01-24 11:03:14 +0545

    Added database functionality

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require('mongoose')
 
+app.get("/", (req, res) => {
+  res.send("This is the home page");
+});
 
-app.get('/', (req, res)=>{
-    res.send("This is the home page")
+mongoose.connect(process.env.DATABASE_URL ).then(()=>{
+    console.log("Connected to database")
+
+    app.listen(process.env.PORT, ()=>{console.log("Listening to Port number 5000")});
+}
+).catch((err)=>{
+    console.log(err.message)
 })
 
-app.listen(5000, 
-    console.log("Listening to Port number 5000")
-)
