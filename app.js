6db6f2a9a00bb1cd6120478cb7ff2222ed499db9
= <=>
2026-01-27 17:24:29 +0545

    Modified in app.js

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.send("This is the home page");
 });
 
-mongoose.connect(process.env.DATABASE_URL ).then(()=>{
-    console.log("Connected to database")
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    console.log("Connected to database");
 
-    app.listen(process.env.PORT, ()=>{console.log("Listening to Port number 5000")});
-}
-).catch((err)=>{
-    console.log(err.message)
-})
-
+    app.listen(process.env.PORT, () => {
+      console.log("Listening to Port number 5000");
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
